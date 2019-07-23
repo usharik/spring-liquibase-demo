fc53b30ecaf34d62aab16a7971f827e54578ca9b
usharik <usharik@mail.ru>
2019-07-23 12:48:44 +0200

    Part 2. https://habr.com/ru/post/460907/

--- src/test/java/ru/usharik/liquibase/demo/LiquibaseDemoApplicationTests.java
+++ src/test/java/ru/usharik/liquibase/demo/LiquibaseDemoApplicationTests.java
@@ -2,11 +2,11 @@ package ru.usharik.liquibase.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
 public class LiquibaseDemoApplicationTests {
 
     @Test
